aac27675abb28f840b5c609a737009e5928b2f24
luizfernando-desenv <luaneataide@gmail.com>
2023-02-18 10:27:47 -0300

    Fechamento aula 01

---
+++ Aula01/script.js
@@ -0,0 +1,68 @@
+console.log("Hello world!")
+{
+    let nome="Luane"
+    //console.log(nome)
+    // let variavel que funciona apenas dentro do escopo
+}
+{
+    var nome2 = Luane
+    // variavel funciona fora do escopo
+}
+console.log(nome2)
+{ // tipos de variáveis
+let n1 = 16
+let n2 = 7.5
+
+let nome4 = "Luane"
+
+let v1 = "true"
+let v2 = "false"
+
+let carros = ["volks","fiat","ford"]
+let data = new Date ("2023-02-12")
+}
+{
+    const PI = 3.14  // nao pode ser alterada
+    let largura = 300
+
+    largura = 400
+
+}
+
+
+// resto
+{
+
+    let n1
+    let result
+
+    n1 = 19
+    result = n1 % 2
+
+    console.log (result)
+}
+{
+    let x = 10
+    let y = 25
+    let result
+
+    result =  x + y
+    console.log ("soma: "+ result)
+
+    result =  x - y
+    console.log ("subtração: "+ result)
+
+    result =  x * y
+    console.log ("multiplicação: "+ result)
+
+    result =  x / y
+    console.log ("divisão: "+ result)
+
+    result =  x % y
+    console.log ("resto: "+ result)
+
+    
+
+
+
+}
